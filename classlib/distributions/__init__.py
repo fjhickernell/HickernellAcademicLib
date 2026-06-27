10c5fef08e6e9bb36c4cfb2bcaaab9ae478293f4
ishn29 <ishaan.hn@gmail.com>
2026-06-27 12:07:46 -0500

    Replaced rv_continuous subclass with SciPyWrapper compatible zero inflated exponential distribution. Added methods for mean, standard deviation, and variance instead of relying on inherited rv_continuous methods .

--- classlib/distributions/__init__.py
+++ classlib/distributions/__init__.py
@@ -1,4 +1,4 @@
-from .zero_inflated_expon import zero_inflated_expon, make_zie
+from .zero_inflated_expon import zero_inflated_expon
 from .uniform_sum import UniformSumDistribution
 
-__all__ = ["zero_inflated_expon", "make_zie", "UniformSumDistribution"]
+__all__ = ["zero_inflated_expon", "UniformSumDistribution"]
